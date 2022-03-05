39bdf8f4afce2d309cffc5a68634d9c092aef5d3
Veljko Mijovic Uskovic <veki.uskovic@gmail.com>
2022-03-05 10:10:22 -0800

    Adding saving to files and image scraping

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import sys
 
 from scraper import Scraper
@@ -8,6 +9,8 @@ keyword = sys.argv[2]
 
 scraper = Scraper(url, keyword)
 
-results = scraper.getLinkedPagesWithKeyword(1)
+#results = scraper.getLinkedPagesWithKeyword(1)
+#scraper.getTextWithKeyword()
+scraper.getImagesFromPage()
 
-print(results)
+#print(json.dumps(results, indent=4))
--- scraper.py
+++ scraper.py
@@ -1,5 +1,8 @@
 from ast import keyword
+from msilib.schema import Error
 from os import system
+import os
+from urllib.request import urlretrieve
 from bs4 import BeautifulSoup
 from flask import request
 import requests
@@ -21,33 +24,60 @@ class Scraper:
         for article in articles:
             if self.keyword in article.text:
                 print(article.text)
+                with open(f"results {document.title.text}.txt","a") as file:
+                    file.write("\n\n\n")
+                    file.write(article.text)
+
         
         for div in divs:
             if self.keyword in div.text:
                 print(div.text)
+                with open(f"results {document.title.text}.txt","a") as file:
+                    file.write("\n\n\n")
+                    file.write(article.text)
+
         
         for par in paragraphs:
             #print(par.decode_contents())
             if self.keyword in par.text:
                 print(par.text)
+                with open(f"results {document.title.text}.txt","a") as file:
+                    file.write("\n\n\n")
+                    file.write(article.text)
 
     def getLinkedPagesWithKeyword(self, depthOfSearch, currentDepth=0, linksDict = {}, url = ""):
-        if(currentDepth==0):
-            document = BeautifulSoup(self.pageRes, "html.parser")
-        else:
-            page = requests.get(url).text
-            document = BeautifulSoup(page, "html.parser")
-        links = document.find_all("a", href=True)
-        title = document.title
-        print(title)
-         
-        for link in links:
-             if self.keyword in link["href"]:
-                 linksDict[title] = link["href"]
-                 if currentDepth < depthOfSearch:
-                     self.getLinkedPagesWithKeyword(depthOfSearch, currentDepth+1, linksDict, link["href"])
+        try:
+            if(currentDepth==0):
+                document = BeautifulSoup(self.pageRes, "html.parser")
+            else:
+                page = requests.get(url).text
+                document = BeautifulSoup(page, "html.parser")
+            links = document.find_all("a", href=True)
+            title = document.title
+            print(title)
+            if title not in linksDict:
+                for link in links:
+                    if self.keyword in link["href"]:
+                        linksDict[title.text] = link["href"]
+                        if currentDepth < depthOfSearch:
+                            self.getLinkedPagesWithKeyword(depthOfSearch, currentDepth+1, linksDict, link["href"])
 
-        currentDepth = currentDepth + 1
+            currentDepth = currentDepth + 1
 
 
-        return linksDict
+            return linksDict    
+        except Exception:
+            print("Cannot get from url: "+ url)
+            return linksDict
+
+    def getImagesFromPage(self):
+        document = BeautifulSoup(self.pageRes, "html.parser")
+        imgs = document.find_all("img")
+        counter = 0
+        for img in imgs:
+            src = img["src"]
+            print(src)
+            if "http" in src:
+                urlretrieve(src, f"{counter}.png")
+                counter = counter + 1
+
